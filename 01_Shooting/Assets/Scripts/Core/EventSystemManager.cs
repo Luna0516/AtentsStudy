9112769b6a6a255df479fba5c5f51c173dd0a308
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2023-12-27 08:36:17 +0900

    01_Shooting - LoadingScene
    - EventSystemManager 를 통해 중복 EventSystem 관리

---
+++ 01_Shooting/Assets/Scripts/Core/EventSystemManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemManager : Singleton<EventSystemManager>
+{
+    protected override void OnPreInitialize()
+    {
+        base.OnPreInitialize();
+        EnsureEventSystemExists();
+    }
+
+    /// <summary>
+    /// 이벤트시스템이 싱글톤으로 잘 만들어 졌는지 확인용 함
+    /// </summary>
+    private void EnsureEventSystemExists()
+    {
+        EventSystem currentEventSystem = FindObjectOfType<EventSystem>();
+
+        if (currentEventSystem == null)
+        {
+            GameObject eventSystemObj = new GameObject("EventSystem");
+            eventSystemObj.AddComponent<EventSystem>();
+            eventSystemObj.AddComponent<StandaloneInputModule>();
+        }
+    }
+}
+
